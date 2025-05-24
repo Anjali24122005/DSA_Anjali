2bde35373fa033e54cbe729d8f3eee24551a78bc
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-05-24 23:12:03 +0530

    Time: 0 ms (100%), Space: 128.2 MB (94.76%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
+        // vector<int>pos;
+        // vector<int>neg;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]>0){
+        //         pos.push_back(nums[i]);
+        //     }
+        //     else{
+        //         neg.push_back(nums[i]);
+        //     }
+        // }
+        // vector<int> result;
+        // int i=0,j=0;
+        // while(i<pos.size() && j<neg.size()){
+        //     result.push_back(pos[i++]);
+        //     result.push_back(neg[j++]);
+        // }
+        //  while (i < pos.size()) {
+        //     result.push_back(pos[i++]);
+        // }
+        // while (j < neg.size()) {
+        //     result.push_back(neg[j++]);
+        // }
+
+        // return result;
+        int n=nums.size();
+        vector<int> result(n);
+        int pos=0, neg=1;
         for(int i=0;i<nums.size();i++){
             if(nums[i]>0){
-                pos.push_back(nums[i]);
+                result[pos]=nums[i];
+                pos+=2;
             }
             else{
-                neg.push_back(nums[i]);
+                result[neg]=nums[i];
+                neg+=2;
             }
-        }
-        vector<int> result;
-        int i=0,j=0;
-        while(i<pos.size() && j<neg.size()){
-            result.push_back(pos[i++]);
-            result.push_back(neg[j++]);
-        }
-         while (i < pos.size()) {
-            result.push_back(pos[i++]);
-        }
-        while (j < neg.size()) {
-            result.push_back(neg[j++]);
-        }
 
+        } 
         return result;
     }
 };
