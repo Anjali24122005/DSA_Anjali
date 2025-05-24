dbe30fb2d898544538cded11641841bcc74fb7e5
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-05-24 23:51:29 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-         int zero = 0, one = 0, two = 0;
+        //  int zero = 0, one = 0, two = 0;
 
-        // Count how many 0s, 1s, and 2s
-        for (int num : nums) {
-            if (num == 0) zero++;
-            else if (num == 1) one++;
-            else two++;
-        }
-
-        // Overwrite the array with counted values
-        int i = 0;
-        while (zero--) nums[i++] = 0;
-        while (one--) nums[i++] = 1;
-        while (two--) nums[i++] = 2;
-        // int low=0,mid=0,high=nums.size()-1;
-        // while(mid<=high){
-        //     if(nums[mid]==0){
-        //         swap(nums[low],nums[mid]);
-        //         low++;
-        //         mid++;
-        //     } else if(nums[mid]==1){
-        //         mid++;
-        //     } else{
-        //         swap(nums[mid],nums[high]);
-        //         high--;
-        //     }
-               
+        // // Count how many 0s, 1s, and 2s
+        // for (int num : nums) {
+        //     if (num == 0) zero++;
+        //     else if (num == 1) one++;
+        //     else two++;
         // }
+
+        // // Overwrite the array with counted values
+        // int i = 0;
+        // while (zero--) nums[i++] = 0;
+        // while (one--) nums[i++] = 1;
+        // while (two--) nums[i++] = 2;
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            } else if(nums[mid]==1){
+                mid++;
+            } else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+               
+        }
     }
 };
