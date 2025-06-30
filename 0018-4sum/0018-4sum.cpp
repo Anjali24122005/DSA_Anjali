692f059fb7fba87d489ae8ad2d8db9a84934d824
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-06-30 17:10:33 +0530

    Time: 1645 ms (5.02%), Space: 508.1 MB (5%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>> st;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<long long> hashset;
+                for(int k=j+1;k<n;k++){
+                    long long sum=nums[i];
+                    sum+=nums[j];
+                    sum+=nums[k];
+                    long long forth=target-sum;
+                    if(hashset.find(forth)!=hashset.end()){
+                        vector<int>temp={nums[i], nums[j], nums[k], (int)forth};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //brute force approach
+        // int n=nums.size();
+        // set<vector<int>> st;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         for(int k=j+1;k<n;k++){
+        //             for(int l=k+1;l<n;l++){
+        //                 long long sum=nums[i]+nums[j];
+        //                 sum+=nums[k];
+        //                 sum+=nums[l];
+
+        //                 if(sum==target){
+        //                     vector<int> temp={nums[i],nums[j], nums[k], nums[l]};
+        //                     sort(temp.begin(),temp.end());
+        //                     st.insert(temp);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(),st.end());
+        // return ans;
+
+    }
+};
