bcd97d78a30e5aaecd630fdf2cf59c5429d5b377
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-06-30 17:19:29 +0530

    Time: 19 ms (81.37%), Space: 17.5 MB (30.45%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,25 +2,35 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
-        set<vector<int>> st;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
         for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                set<long long> hashset;
-                for(int k=j+1;k<n;k++){
+                if(j>i+1 && nums[j]== nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
                     long long sum=nums[i];
                     sum+=nums[j];
                     sum+=nums[k];
-                    long long forth=target-sum;
-                    if(hashset.find(forth)!=hashset.end()){
-                        vector<int>temp={nums[i], nums[j], nums[k], (int)forth};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+                    sum+=nums[l];
+
+                    if(sum==target){
+                        vector<int>temp={nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        l--;
+
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
-                    hashset.insert(nums[k]);
+                    else if(sum<target) k++;
+                    else l--;
+
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
 
 
@@ -31,15 +41,28 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-
+        //better
+        // int n=nums.size();
+        // set<vector<int>> st;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         set<long long> hashset;
+        //         for(int k=j+1;k<n;k++){
+        //             long long sum=nums[i];
+        //             sum+=nums[j];
+        //             sum+=nums[k];
+        //             long long forth=target-sum;
+        //             if(hashset.find(forth)!=hashset.end()){
+        //                 vector<int>temp={nums[i], nums[j], nums[k], (int)forth};
+        //                 sort(temp.begin(), temp.end());
+        //                 st.insert(temp);
+        //             }
+        //             hashset.insert(nums[k]);
+        //         }
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin(), st.end());
+        // return ans;
 
         //brute force approach
         // int n=nums.size();
