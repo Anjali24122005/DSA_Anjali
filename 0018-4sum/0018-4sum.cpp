6844c345b707e3b20a20875e7c57dfd77a0ebfcf
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-06-30 17:21:53 +0530

    Time: 25 ms (35.34%), Space: 17.4 MB (58.54%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -18,9 +18,9 @@ public:
                     long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
 
                     if(sum==target){
-                        // vector<int>temp={nums[i], nums[j], nums[k], nums[l]};
-                        // ans.push_back(temp);
-                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        vector<int>temp={nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        // ans.push_back({nums[i], nums[j], nums[k], nums[l]});
 
                         k++;
                         l--;
