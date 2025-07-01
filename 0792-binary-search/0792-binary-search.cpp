fb67accbf9d772eac3da08ae2519e5c1c4f0d796
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2025-07-01 22:43:46 +0530

    Time: 0 ms (100%), Space: 31.3 MB (79.86%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
-    int binary(vector<int> & nums, int start, int end, int target){
-        if(start>end) return -1;
+    // int binary(const vector<int> & nums, int start, int end, int target){
+    //     if(nums.empty()) return -1;
+    //     if(start>end) return -1;
 
-        int mid=start+(end-start)/2;
+    //     int mid=start+(end-start)/2;
 
-        if(nums[mid]==target) return mid;
-        else if(nums[mid]<target){
-            return binary(nums, mid+1, end, target);
-        }else{
-            return binary(nums, start, mid-1, target);
-        }
-    }
+    //     if(nums[mid]==target) return mid;
+    //     else if(nums[mid]<target){
+    //         return binary(nums, mid+1, end, target);
+    //     }else{
+    //         return binary(nums, start, mid-1, target);
+    //     }
+    // }
     int search(vector<int>& nums, int target) {
-        return binary(nums, 0, nums.size()-1, target);
+        int start=0;
+        int end=nums.size()-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<target){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
     }
 };
