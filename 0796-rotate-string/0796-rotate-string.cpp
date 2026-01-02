ff38f658251d7c26ad259d427eddb431bd4cd702
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-01-02 22:16:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.12%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -3,10 +3,13 @@ public:
     bool rotateString(string s, string goal) {
         if(s.length()!=goal.length()) return false;
 
-        for(int i=0;i<s.size();i++){
-            string rotated=s.substr(i)+s.substr(0,i);
-            if(rotated==goal) return true;
-        }
-        return false;
+        // for(int i=0;i<s.size();i++){
+        //     string rotated=s.substr(i)+s.substr(0,i);
+        //     if(rotated==goal) return true;
+        // }
+        // return false;
+
+        string doubledS=s+s;
+        return doubledS.find(goal)!=string::npos;
     }
 };
