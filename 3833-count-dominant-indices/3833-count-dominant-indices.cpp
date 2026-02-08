cf0dc66ed1d2bfe1396848f9823fd2bf48e5c6e3
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-02-08 20:49:58 +0530

    Time: 1 ms (20.22%), Space: 32.3 MB (51.61%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int dominantIndices(vector<int>& nums) {
+        int n=nums.size();
+        int dominant=0;
+        for(int i=0;i<n;i++){
+            int sum=0, cnt=0;
+            for(int j=i+1;j<n;j++){
+                sum+=nums[j];
+                cnt++;
+            }
+            double avg=(double)sum/cnt;
+
+            if(nums[i]>avg){
+                dominant++;
+            }
+        }
+        return dominant;
+    }
+};
