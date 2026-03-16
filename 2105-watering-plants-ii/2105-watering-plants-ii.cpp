d0b3d6723a05875f38e73418e94c081b812b7580
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-16 20:14:17 +0530

    Time: 0 ms (100%), Space: 82 MB (93.35%) - LeetHub

--- 2105-watering-plants-ii/2105-watering-plants-ii.cpp
+++ 2105-watering-plants-ii/2105-watering-plants-ii.cpp
@@ -1,54 +1,82 @@
 class Solution {
 public:
     int minimumRefill(vector<int>& plants, int capacityA, int capacityB) {
-        int refill=0;
-        int n=plants.size();
-        int a=0, b=n-1;
-        int cana=capacityA, canb=capacityB;
-        while(a<b){
-            if(plants[a]<=cana){
-                cana-=plants[a];
-                a++;
-            }else{
-                cana=capacityA;
-                cana-=plants[a];
-                refill++;
-                a++;
-            }
-            if(plants[b]<=canb){
-                canb-=plants[b];
-                b--;
-            }else{
-                canb=capacityB;
-                canb-=plants[b];
-                refill++;
-                b--;
-            }
-            if(a==b){
-                if(canb>cana){
-                    if(plants[b]<=canb){
-                        canb-=plants[b];
-                        b--;
-                    }else{
-                        canb=capacityB;
-                        canb-=plants[b];
-                        refill++;
-                        b--;
-                    }
-                }
-                else{
-                    if(plants[a]<=cana){
-                        cana-=plants[a];
-                        a++;
-                    }else{
-                        cana=capacityA;
-                        cana-=plants[a];
-                        refill++;
-                        a++;
-                    }
-                }
-            }
+        // int refill=0;
+        // int n=plants.size();
+        // int a=0, b=n-1;
+        // int cana=capacityA, canb=capacityB;
+        // while(a<b){
+        //     if(plants[a]<=cana){
+        //         cana-=plants[a];
+        //         a++;
+        //     }else{
+        //         cana=capacityA;
+        //         cana-=plants[a];
+        //         refill++;
+        //         a++;
+        //     }
+        //     if(plants[b]<=canb){
+        //         canb-=plants[b];
+        //         b--;
+        //     }else{
+        //         canb=capacityB;
+        //         canb-=plants[b];
+        //         refill++;
+        //         b--;
+        //     }
+        //     if(a==b){
+        //         if(canb>cana){
+        //             if(plants[b]<=canb){
+        //                 canb-=plants[b];
+        //                 b--;
+        //             }else{
+        //                 canb=capacityB;
+        //                 canb-=plants[b];
+        //                 refill++;
+        //                 b--;
+        //             }
+        //         }
+        //         else{
+        //             if(plants[a]<=cana){
+        //                 cana-=plants[a];
+        //                 a++;
+        //             }else{
+        //                 cana=capacityA;
+        //                 cana-=plants[a];
+        //                 refill++;
+        //                 a++;
+        //             }
+        //         }
+        //     }
 
+        // }
+        // return refill;
+        int n=plants.size();
+        int left=0;
+        int right=n-1;
+        int cana=capacityA, canb=capacityB;
+        int refill=0;
+
+        while(left<right){
+            if(cana<plants[left]){
+                cana=capacityA;
+                refill++;
+            }
+            cana-=plants[left];
+            left++;
+
+            if(canb<plants[right]){
+                canb=capacityB;
+                refill++;
+            }
+            canb-=plants[right];
+            right--;
+        }
+
+        if(left==right){
+            if(max(cana,canb)<plants[left]){
+                refill++;
+            }
         }
         return refill;
     }
