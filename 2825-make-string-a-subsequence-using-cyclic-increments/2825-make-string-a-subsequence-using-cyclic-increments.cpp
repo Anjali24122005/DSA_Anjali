fb23dcd65f72033a671db988985cc761de6ed6fd
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-21 19:08:17 +0530

    Time: 8 ms (25.25%), Space: 17.1 MB (72.46%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
+        if(str1.size()<str2.size()) return false;
         int l1=0, l2=0;
         while(l1<str1.size() && l2<str2.size()){
             if(str2[l2]==str1[l1]){
                 l1++;
                 l2++;
             }else{
-                char cn;
+                char cyclicnext;
                 if(str1[l1]=='z'){
-                    cn='a';
+                    cyclicnext='a';
                 }else {
-                    cn=str1[l1]+1;
+                    cyclicnext=str1[l1]+1;
                 }
 
-                if(str2[l2]==cn){
+                if(str2[l2]==cyclicnext){
                     l1++;
                     l2++;
                 }else{
