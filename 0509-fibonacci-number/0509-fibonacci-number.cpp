5d7d2bc93c5e5817cf0f198511a1d79530f88fe0
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-27 13:42:55 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.48%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int fibbo(int n, vector<int>&v){
+    // int fibbo(int n, vector<int>&v){
+    //     if(n<=1){
+    //         return n;
+    //     }
+    //     if(v[n]!=-1){
+    //         return v[n];
+    //     }
+
+    //     v[n]=fibbo(n-1, v)+fibbo(n-2,v);
+    //     return v[n];
+    //}
+    int fib(int n) {
+        // vector<int>memo(n+1,-1);
+        // return fibbo(n,memo);
         if(n<=1){
             return n;
         }
-        if(v[n]!=-1){
-            return v[n];
-        }
 
-        v[n]=fibbo(n-1, v)+fibbo(n-2,v);
-        return v[n];
-    }
-    int fib(int n) {
-        vector<int>memo(n+1,-1);
-        return fibbo(n,memo);
+        vector<int>dp(n+1);
+
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
