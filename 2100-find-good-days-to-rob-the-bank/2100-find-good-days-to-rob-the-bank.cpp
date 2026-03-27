ccb08fd1f63d32c58292e5a41434c182a92b95e4
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-27 21:03:42 +0530

    Time: 7 ms (63.72%), Space: 91.2 MB (92.42%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -2,23 +2,45 @@ class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
         int n=security.size();
-        vector<int>left(n,0);
-        vector<int>right(n,0);
+        // vector<int>left(n,0);
+        // vector<int>right(n,0);
 
-        for(int i=1;i<n;i++){
-            if(security[i]<=security[i-1]){
-                left[i]=left[i-1]+1;
-            }
-        }
-        for(int i=n-2;i>=0;i--){
+        // for(int i=1;i<n;i++){
+        //     if(security[i]<=security[i-1]){
+        //         left[i]=left[i-1]+1;
+        //     }
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     if(security[i]<=security[i+1]){
+        //         right[i]=right[i+1]+1;
+        //     }
+        // }
+        // vector<int>ans;
+
+        // for(int i=0;i<n;i++){
+        //     if(left[i]>=time && right[i]>=time){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        vector<int>right(n,0);
+        for(int i=n-2;i>-0;i--){
             if(security[i]<=security[i+1]){
                 right[i]=right[i+1]+1;
             }
         }
         vector<int>ans;
+        int left=0;
 
         for(int i=0;i<n;i++){
-            if(left[i]>=time && right[i]>=time){
+            if(i>0 && security[i]<=security[i-1]){
+                left++;
+            }else{
+                left=0;
+            }
+
+            if(left>=time && right[i]>=time){
                 ans.push_back(i);
             }
         }
