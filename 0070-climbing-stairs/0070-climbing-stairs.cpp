bcc4dcd9a4759904f02b9e22219144562c1d1411
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-27 15:10:11 +0530

    Time: 2 ms (4.44%), Space: 8.5 MB (33.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -18,12 +18,12 @@ public:
         // return left+right;
 
         vector<int>dp(n+1);
-        if(n<=1) return n;
+        if(n<=2) return n;
 
-        dp[0]=1;
         dp[1]=1;
+        dp[2]=2;
 
-        for(int i=2;i<=n;i++){
+        for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
