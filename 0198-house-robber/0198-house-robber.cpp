7e209e8844e21890dd1770f3bce083a729777c90
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-28 19:50:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (43.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-
+    int f(int ind , vector<int>&nums, vector<int>&memo){
         int n=nums.size();
-        if(n == 0) return 0;
-        int prev=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1) take+=prev2;
-            int nontake=0+prev;
-            int curi=max(take,nontake);
-            prev2=prev;
-            prev=curi;
-        }
-        return prev;
+
+        if(ind==0) return nums[ind];
+        if(ind<0) return 0;
+
+        if(memo[ind]!=-1) return memo[ind];
+        
+
+        int pick=nums[ind]+f((ind-2), nums, memo);
+        int notpick=0+f((ind-1), nums, memo);
+
+        return memo[ind]=max(pick, notpick);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> memo(n,-1);
+        return f(n-1, nums, memo);
+        // int n=nums.size();
+        // if(n == 0) return 0;
+        // int prev=nums[0];
+        // int prev2=0;
+        // for(int i=1;i<n;i++){
+        //     int take=nums[i];
+        //     if(i>1) take+=prev2;
+        //     int nontake=0+prev;
+        //     int curi=max(take,nontake);
+        //     prev2=prev;
+        //     prev=curi;
+        // }
+        // return prev;
     }
 };
