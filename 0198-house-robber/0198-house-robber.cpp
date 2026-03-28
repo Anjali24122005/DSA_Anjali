847c28d211a7c379ef33ea681df7a3402e38afd1
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-28 20:02:50 +0530

    Time: 1 ms (9.12%), Space: 10.6 MB (43.4%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int f(int ind , vector<int>&nums, vector<int>&memo){
-        int n=nums.size();
+    // int f(int ind , vector<int>&nums, vector<int>&memo){
+    //     int n=nums.size();
 
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
+    //     if(ind==0) return nums[ind];
+    //     if(ind<0) return 0;
 
-        if(memo[ind]!=-1) return memo[ind];
+    //     if(memo[ind]!=-1) return memo[ind];
         
 
-        int pick=nums[ind]+f((ind-2), nums, memo);
-        int notpick=0+f((ind-1), nums, memo);
+    //     int pick=nums[ind]+f((ind-2), nums, memo);
+    //     int notpick=0+f((ind-1), nums, memo);
 
-        return memo[ind]=max(pick, notpick);
-    }
+    //     return memo[ind]=max(pick, notpick);
+    // }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> memo(n,-1);
-        return f(n-1, nums, memo);
+        // int n=nums.size();
+        // vector<int> memo(n,-1);
+        // return f(n-1, nums, memo);
+
+
         // int n=nums.size();
         // if(n == 0) return 0;
         // int prev=nums[0];
@@ -31,5 +33,22 @@ public:
         //     prev=curi;
         // }
         // return prev;
+        int n=nums.size();
+        vector<int>dp(n,0);
+
+        if(n<=0) return 0;
+
+        dp[0]=nums[0];
+        
+        for(int i=1;i<n;i++){
+            if(i<2) dp[i]=max(dp[i-1], nums[i]);
+            else dp[i]=max(dp[i-1], nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 };
+
+// array=[1,2,3,4]
+// dp=[1,2,4,6]
+// dp[2]=max(2,4) dp[2]=4
+// dp[3]=max(4,6)
