683d5dd83312f91a9a7fe62d77dd942cdbcf6d52
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-28 21:47:03 +0530

    Time: 38 ms (78.88%), Space: 53.7 MB (51.02%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return "Hello World";
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
