2ff2d6bd0a2c3ce5a79be49e0d83841e780e2c12
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-31 21:56:40 +0530

    Time: 0 ms (100%), Space: 8.1 MB (84.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -13,20 +13,28 @@ public:
         // vector<vector<int>>dp(m+1, vector<int>(n+1,-1));
         // return f(m,n,dp);
 
-        vector<vector<int>>dp(m+1, vector<int>(n+1,0));
-        // if(m==0 || n==0) return 0;
-        // dp[1][1]=1;
-        for(int i=1;i<m+1;i++){
-            for(int j=1;j<n+1;j++){
-                if(i==1 && j==1)dp[i][j]=1;
-                else{
-                int bottom=0, right=0;
-                if(i>1) bottom = dp[i-1][j];
-                if(j>1) right=dp[i][j-1];
-                dp[i][j]=bottom+right;
-                }
+        // vector<vector<int>>dp(m+1, vector<int>(n+1,0));
+        // // if(m==0 || n==0) return 0;
+        // // dp[1][1]=1;
+        // for(int i=1;i<m+1;i++){
+        //     for(int j=1;j<n+1;j++){
+        //         if(i==1 && j==1)dp[i][j]=1;
+        //         else{
+        //         int bottom=0, right=0;
+        //         if(i>1) bottom = dp[i-1][j];
+        //         if(j>1) right=dp[i][j-1];
+        //         dp[i][j]=bottom+right;
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
+
+        vector<int>dp(n,1);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[j]=dp[j]+dp[j-1];//1+1=2  1+
             }
         }
-        return dp[m][n];
+        return dp[n-1];
     }
 };
