12e03080394d6832ca366a4cff3cf474bf535849
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-03-31 23:01:43 +0530

    Time: 0 ms (100%), Space: 11.6 MB (83.37%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -11,28 +11,43 @@ public:
     // }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         
+        // int m = obstacleGrid.size();
+        // int n = obstacleGrid[0].size();
+        
+        // vector<vector<int>>dp(m, vector<int>(n, 0));
+        // if(obstacleGrid[0][0] == 1) return 0;
+        // dp[0][0] = 1;
+        //  for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(obstacleGrid[i][j] == 1){
+        //             dp[i][j] = 0;
+        //             continue;
+        //         }
+        //         if(i == 0 && j == 0) continue;
+
+        //         int right=0, bottom=0;
+        //         if(i>0) bottom=dp[i-1][j];
+        //         if(j>0) right=dp[i][j-1];
+        //         dp[i][j]=bottom+right;
+        //     }
+        //  }
+        // // return f(m-1,n-1,obstacleGrid,dp);
+        // return dp[m-1][n-1];
+
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        
-        vector<vector<int>>dp(m, vector<int>(n, 0));
-        if(obstacleGrid[0][0] == 1) return 0;
-        dp[0][0] = 1;
-         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i == 0 && j == 0) continue;
+        vector<int>dp(n,0);
+        dp[0]=(obstacleGrid[0][0]==0)?1:0;
 
-                int right=0, bottom=0;
-                if(i>0) bottom=dp[i-1][j];
-                if(j>0) right=dp[i][j-1];
-                dp[i][j]=bottom+right;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1) dp[j]=0;
+                else if(j>0){
+                    dp[j]=dp[j]+dp[j-1];
+                }
             }
-         }
-        // return f(m-1,n-1,obstacleGrid,dp);
-        return dp[m-1][n-1];
+        }
+        return dp[n-1];
     }
 };
 
