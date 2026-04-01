1f1789a42246893a3ef1b3753b921464fa91506d
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-04-01 15:01:17 +0530

    Time: 0 ms (100%), Space: 13.5 MB (22.22%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -15,16 +15,23 @@ public:
         // return f(0,0,triangle,n,dp);
 
         int n= triangle.size();
-        vector<int> dp=triangle[n-1];
+        // vector<int> dp=triangle[n-1];
+
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=0;j<=i;j++){
+        //         dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
+        //     }
+        // }
+        // return dp[0];
+        
+        vector<vector<int>>dp=triangle;
 
         for(int i=n-2;i>=0;i--){
             for(int j=0;j<=i;j++){
-                dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
+                dp[i][j]+=min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
-        return dp[0];
-
-        
+        return dp[0][0];
     }
 };
 
