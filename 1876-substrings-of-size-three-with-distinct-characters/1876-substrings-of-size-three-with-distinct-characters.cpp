0cc7e0944bed09a536c7a1de734991352a2fee91
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-04-10 23:01:01 +0530

    Time: 7 ms (5.91%), Space: 11 MB (5.02%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,12 +1,28 @@
+// class Solution {
+// public:
+//     int countGoodSubstrings(string s) {
+//         if(s.size()<3) return 0;
+//         int count = 0;
+//         for(int i = 0; i <= s.length() - 3; i++) {
+//             if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i] != s[i+2]) {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int countGoodSubstrings(string s) {
         if(s.size()<3) return 0;
         int count = 0;
         for(int i = 0; i <= s.length() - 3; i++) {
-            if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i] != s[i+2]) {
-                count++;
-            }
+            unordered_set<char> st;
+            st.insert(s[i]);
+            st.insert(s[i+1]);
+            st.insert(s[i+2]);
+            if(st.size() == 3) count++;
         }
         return count;
     }
