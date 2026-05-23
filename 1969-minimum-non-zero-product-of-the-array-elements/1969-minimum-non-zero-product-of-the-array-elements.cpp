0b74ee228ed196991bca37c3a1fa1b191f709329
Anjali24122005 <165129004+Anjali24122005@users.noreply.github.com>
2026-05-23 09:35:58 +0530

    Time: 0 ms (100%), Space: 7.7 MB (71.92%) - LeetHub

---
+++ 1969-minimum-non-zero-product-of-the-array-elements/1969-minimum-non-zero-product-of-the-array-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    const long long MOD=1e9+7;
+    long long power(long long x, long long n){
+        long long ans=1;
+        x = x % MOD;
+        if(n==0) return 1;
+
+        while(n>0){
+            if(n%2==1){
+                ans=(ans*x)%MOD;
+            }
+            x=(x*x)%MOD;
+            n/=2;
+        }
+        return ans;
+    }
+    int minNonZeroProduct(int p) {
+        long long maxnum=(1LL<<p)-1;
+        long long secmax=maxnum-1;
+        long long exponent=(1LL<<(p-1))-1;
+
+        return (power(secmax, exponent) * (maxnum % MOD)) % MOD;
+    }
+};
